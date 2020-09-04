dc86f9e01ca6e157f221fb1a505625244e6136b0
Chester Lau <chesterlau@live.com>
2020-09-04 16:35:17 +1000

    Fixed up cosmosdb error

--- src/Models/Todo.cs
+++ src/Models/Todo.cs
@@ -1,7 +1,10 @@
-﻿namespace TodoAppMicroservice.Models
+﻿using Newtonsoft.Json;
+
+namespace TodoAppMicroservice.Models
 {
     public class Todo
     {
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
 
         public string Name { get; set; }
